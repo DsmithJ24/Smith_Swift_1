57a8f6fdac21efa71bc5fb40ac8f8d5f9da9da2b
RafaGM24 <DsmithJ24@gmail.com>
2021-02-13 14:30:15 -0500

    Beginning of Sprint 2. Data will soon be saved into DB

--- Demo.py
+++ Demo.py
@@ -1,47 +1,50 @@
 import secrets
 import requests
-import json
-#when using debugger, wait a sec for everything to load
+# import json
+
+import sqlite3
+from typing import Tuple
+
+DATA_MINIMUM = 1000
+
+# when using debugger, wait a sec for everything to load
 url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?"
 outfile = open(r"api_data.txt", "w")
 
 def get_data():
-    #will retrieve:
-        #school name
-        #school city
-        #2018 student size
-        #2017 student size
-        #2017 earnings (3 years after completion.overall count over poverty line)
-        #2016 repayment (3 year repayment.overall)
+    # will retrieve:
+    # school name
+    # school city
+    # 2018 student size
+    # 2017 student size
+    # 2017 earnings (3 years after completion.overall count over poverty line)
+    # 2016 repayment (3 year repayment.overall)
 
     all_data = []
-    #gets: 3203 elements and 20 per page. will need 161 pages
-    #for page in range(162):
     page = 0
-    NextPage = True
+    nextPage = True
 
-    #Stops if it hits a false
-    while NextPage != False:
-        #print(page)
-        #put additional fields after &fields=
-        response = requests.get(f"{url}school.degrees_awarded.predominant=2,3&fields=school.name,school.state,"
+    # Stops if it hits a false
+    while nextPage != False:
+        # print(page)
+        # put additional fields after &fields=
+        response = requests.get(f"{url}school.degrees_awarded.predominant=2,3&fields=school.name,school.city,"
                                 f"2018.student.size,2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,"
                                 f"2016.repayment.3_yr_repayment.overall&api_key={secrets.api_key}&page={page}") #put the api url here
         if response.status_code != 200:
             print("Error getting data!")
-            #exit(-1)
-            #do not exit, continue
+            # do not exit, continue. Don't get stuck at one point
             page = page + 1
             continue
 
-        #following code puts data in .json format
+        # following code puts data in .json format
         page_of_data = response.json()
         page_of_school_data = page_of_data['results']
         all_data.extend(page_of_school_data)
-        NextPage = check_page(page_of_school_data)
+        nextPage = check_page(page_of_school_data)
         page=page+1
-    print("Total number of pages is", page)
-    return all_data
+    # print("Total number of pages is", page)
+    return all_data, page
 
 def check_page(page):
     if len(page) < 20:
@@ -52,13 +55,73 @@ def check_page(page):
     else:
         pass
 
-#takes the retrieved data and saves it to a .txt file
+def check_data_entries(data, pages):
+    print("Total number of pages is", pages)
+    length = len(data)
+    if length < DATA_MINIMUM:
+        print(f"Less than {DATA_MINIMUM} entries! Ending")
+        # end program?
+        exit(-1)
+
+def open_DB(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
+    # Create a DB if one does not exist (will do this always for now), or connect to one
+    db_connection = sqlite3.connect(filename)
+    # prepare to read/write data
+    cursor = db_connection.cursor()
+    return db_connection, cursor
+
+def close_DB(connection:sqlite3.Connection):
+    # save any changes
+    connection.commit()
+    # close the connection
+    connection.close()
+
+def setup_DB(cursor:sqlite3.Cursor):
+    # create columns labeled after the 6 parameters
+    # name DB after schools. Can't start with numbers
+    # may need to initalize with '?' (?)
+    cursor.execute('''CREATE TABLE IF NOT EXISTS schools(
+    school_name TEXT NOT NULL,
+    school_city TEXT NOT NULL,
+    student_size_2018 INTEGER DEFAULT 0,
+    student_size_2017 INTEGER DEFAULT 0,
+    over_poverty_after_3_years_2017 REAL DEFAULT 0,
+    repayment_overall_2016 REAL DEFAULT 0
+    );''')
+
+def store_In_DB(api_data, cursor:sqlite3.Cursor):
+    # takes the fetched data and stores in a DB
+    # store data in the DB (while loop), use the all_data array?
+
+    # use the dictionary tiles to put in DB (no while loop?)
+    cursor.execute(f'''INSERT INTO SCHOOLS 
+    (school_name, school_city, student_size_2018, student_size_2017, over_poverty_after_3_years_2017, repayment_overall_2016)
+    VALUES ({api_data['school.name']},
+    {api_data['school.city']},
+    {api_data['2018.student.size']},
+    {api_data['2017.student.size']},
+    {api_data['2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line']},
+    {api_data['2016.repayment.3_yr_repayment.overall']}
+    )''')
+
 def main():
-    demo_data = get_data()
+    # saves the data to a new table, used this to save into DB
+    demo_data, pages = get_data()
+    # test to see if >1000 entries. If not, exit
+    check_data_entries(demo_data, pages)
+    '''
+    #these next few lines are for an older version. May still want them in comments
     print(json.dumps(demo_data, indent=6), file=outfile)
     #will create a .txt file with 25626 lines. 25626/8 = 3203 entries as expected
     outfile.close()
     print("Data has been saved to a file")
+    '''
+
+    conn, cursor = open_DB("demo_db.sqlite")
+    setup_DB(cursor)
+    store_In_DB(demo_data, cursor)
+    print(type(conn))
+    close_DB(conn)
 
 if __name__ == '__main__':
     main()
