891457cf3c8fdb755f3198191a9ae6b9de6c4a5f
RafaGM24 <DsmithJ24@gmail.com>
2021-03-01 00:18:32 -0500

    All test work. Tests 2 and 3 are merged into one

--- Data.py
+++ Data.py
@@ -1,14 +1,24 @@
+
 import secrets
 import requests
-# import json
+import json
 
 import sqlite3
 from typing import Tuple
 
+import pandas as pd
+import os
+# need to add json, pandas, requests, and os to requirements
+
 url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?"
 # outfile = open(r"api_data.txt", "w")
 
-def get_data():
+# prevents error reading the file
+pre = os.path.dirname(os.path.realpath(__file__))
+filename = "state_M2019_dl.xlsx"
+path = os.path.join(pre, filename)
+
+def get_api_data():
     # will retrieve:
     # school name
     # school city
@@ -30,7 +40,7 @@ def get_data():
                                 f"2016.repayment.3_yr_repayment.overall&api_key={secrets.api_key}&page={page}") #put the api url here
         if response.status_code != 200:
             print("Error getting data!")
-            # do not exit, continue. Don't get stuck at one point
+            # do not exit, continue. Don't get stuck at one point. Rather miss data than endless loop
             page = page + 1
             continue
 
@@ -41,9 +51,30 @@ def get_data():
         nextPage = check_page(page_of_school_data)
         page=page+1
     # print("Total number of pages is", page)
-    return all_data, page
+    return all_data
+
+def get_excel_data():
+    # area_type 2 is for state, 3 is for territory
+    # o group must be 'major', bad options are total and detailed
+
+    # need:
+    # state, occupation major title, total employment in that field in that state
+    # 25th percentile salary (assume college grads in lower 25%) for field both hourly AND annually
+    # and occupation code
+
+    excel = pd.read_excel(path, engine='openpyxl')
+
+    # keep the desired data
+    df = pd.DataFrame(excel, columns=['area_title', 'occ_title', 'o_group', 'tot_emp', 'h_pct25', 'a_pct25', 'occ_code'])
+    df = df[df.o_group == 'major']
+
+    # put into json format like api data
+    data = df.to_json(orient='records')
+    parsed_data = json.loads(data)
+    return parsed_data
 
 def check_page(page):
+    # checks to see if api data fills a page, if not then all data has been collected
     if len(page) < 20:
         return False
     elif len(page) > 20:
@@ -65,7 +96,7 @@ def close_DB(connection:sqlite3.Connection):
     # close the connection
     connection.close()
 
-def setup_DB(cursor:sqlite3.Cursor):
+def setup_DB_schools(cursor:sqlite3.Cursor):
     # create columns labeled after the 6 parameters
     # name DB after schools. Can't start with numbers
     # first 2 are strings,when entered, must have ?
@@ -79,42 +110,77 @@ def setup_DB(cursor:sqlite3.Cursor):
     repayment_overall_2016 INTEGER DEFAULT 0
     );''')
 
-def store_In_DB(api_data, cursor:sqlite3.Cursor):
-    # use the dictionary tiles to put in DB (no while loop?)
+def setup_DB_jobs(cursor:sqlite3.Cursor):
+    # need:
+    # state, occupation major title, total employment in that field in that state
+    # 25th percentile salary (assume college grads in lower 25%) for field both hourly and annually
+    # and occupation code
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS occupation(
+    state_name TEXT NOT NULL,
+    occupation_title TEXT NOT NULL,
+    employment_in_field INTEGER,
+    hour_salary_25th_percentile DOUBLE,
+    annual_salary_25th_percentile INTEGER,
+    occupation_code TEXT NOT NULL
+    );''')
+
+def check_data(data: int):
+    # checks to make sure data with expected integers have 0s instead of None
+    # check to see if int values are null
+    # maybe just replace value with 0, cannot do elif. Must be ifs
+
+    if data == None:
+        data = 0
+        return data
+
+def store_In_DB(api_data: list, excel_data: list, cursor:sqlite3.Cursor):
+    # use the dictionary tiles to put in DB
     # use the ? method, put ? into inputted values and then the dictionary values outside ()
     # this way, the data is a string and not thought of as a column name
 
-    # use while loop
-    entry = 0
-    while entry < len(api_data):
+    # use for loop
+    for adata in api_data:
+        # first check to see if any unwanted Nulls
+
+        '''
+        check_data(adata['2018.student.size'])
+        check_data(adata['2017.student.size'])
+        check_data(adata['2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line'])
+        check_data('2016.repayment.3_yr_repayment.overall')
+        '''
         # have list[x][param] in the Tuple (after VALUES) where x is entry and para is the column
-        # check to see if int values are null
-        # maybe just replace value with 0, cannot do elif. Must be ifs
-        if api_data[entry]['2018.student.size'] == None:
-            api_data[entry]['2018.student.size'] = 0
-
-        if api_data[entry]['2017.student.size'] == None:
-            api_data[entry]['2017.student.size'] = 0
-
-        if api_data[entry]['2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line'] == None:
-            api_data[entry]['2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line'] = 0
-
-        if api_data[entry]['2016.repayment.3_yr_repayment.overall'] == None:
-            api_data[entry]['2016.repayment.3_yr_repayment.overall'] = 0
-
         # insert data, need as many ?s as columns
-        cursor.execute('''INSERT INTO SCHOOLS (school_name, school_city, 
+        cursor.execute('''INSERT INTO schools(school_name, school_city, 
         student_size_2018, student_size_2017, over_poverty_after_3_years_2017, repayment_overall_2016)
         VALUES (?,?,?,?,?,?)''',
-                       (api_data[entry]['school.name'], api_data[entry]['school.city'],
-                        api_data[entry]['2018.student.size'], api_data[entry]['2017.student.size'],
-                        api_data[entry]['2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line'],
-                        api_data[entry]['2016.repayment.3_yr_repayment.overall']))
-        entry = entry + 1
+                       (adata['school.name'], adata['school.city'],
+                        adata['2018.student.size'], adata['2017.student.size'],
+                        adata['2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line'],
+                        adata['2016.repayment.3_yr_repayment.overall']))
+
+    for edata in excel_data:
+
+        # want one last check to see if any non major o_groups have snuck in
+        # this check just wiped the data...
+        if edata['o_group'] == 'major':
+            cursor.execute('''INSERT INTO occupation(state_name, occupation_title,
+            employment_in_field, hour_salary_25th_percentile, annual_salary_25th_percentile,
+            occupation_code) VALUES(?,?,?,?,?,?)''',
+                            (edata['area_title'], edata['occ_title'], edata['tot_emp'],
+                            edata['h_pct25'], edata['a_pct25'], edata['occ_code']))
 
 def main():
-    # saves the data to a new table, used this to save into DB
-    demo_data, pages = get_data()
+    # check to see if DB file exists. Delete it to prevent duplicate data
+    if os.path.exists("sprint_db.sqlite"):
+        os.remove("sprint_db.sqlite")
+
+    api_data = get_api_data()
+
+    print("Api data retrieved!")
+
+    excel_data = get_excel_data()
+    print("Excel data retrieved! Storing data...")
 
     '''
     # these next few lines are for an older version. May still want them in comments
@@ -122,16 +188,21 @@ def main():
     # will create a .txt file with 25626 lines. 25626/8 = 3203 entries as expected
     outfile.close()
     print("Data has been saved to a file")
+    # print(json.dumps(excel_data, indent=6), file=outfile)
+    # outfile.close()
     '''
 
-    conn, cursor = open_DB("demo_db.sqlite")
-    setup_DB(cursor)
-    print(type(conn))
 
-    store_In_DB(demo_data, cursor)
+    conn, cursor = open_DB("sprint_db.sqlite")
+    setup_DB_schools(cursor)
+    setup_DB_jobs(cursor)
+
+    store_In_DB(api_data, excel_data, cursor)
     print("Data has been stored in the Database!")
 
     close_DB(conn)
 
+
 if __name__ == '__main__':
     main()
+
--- Test/test_sprint2.py
+++ Test/test_sprint2.py
@@ -2,23 +2,106 @@ import Data
 
 DATA_MINIMUM = 1000
 
-def test_data_entries():
-    data = Data.get_data()
-    assert len(data) <= DATA_MINIMUM
 
+def test_data_entries():
+    data = Data.get_excel_data()
+    assert len(data) >= DATA_MINIMUM
+
+def test_states():
+    data = Data.get_excel_data()
+    states = []
+    for entries in data:
+        if entries['area_title'] in states:
+            pass
+        else:
+            states.append(entries['area_title'])
+    # all 50?
+    assert len(states) >= 50
+    # includes territories +DC too
+    assert len(states) <= 54
+
+def test_table_names():
+    # test to see if new table exists
+    # test to see if old table exists
+    conn, cursor = Data.open_DB("test_db.sqlite")
+    Data.setup_DB_schools(cursor)
+    Data.setup_DB_jobs(cursor)
+
+    cursor.execute('''SELECT name FROM sqlite_master WHERE type='table';''')
+
+    res = cursor.fetchall()
+
+    assert len(res) == 2
+
+    test_res = res[0]
+    assert test_res[0] == 'schools'
+    test_res = res[1]
+    assert test_res[0] == 'occupation'
 
 def test_store_data():
-    conn, cursor = Data.open_DB("demo_db.sqlite")
-    Data.setup_DB(cursor)
+    # this is a modified version of the shared answer for Sprint 2 with the additional field for
+    # excel data since the store function is used for both and takes both parameters, test from
+    # before will be the same. Test for the jobs data will be very similar.
 
-    data = Data.get_data()
-    Data.store_In_DB(data, cursor)
+    # make the database first
+    conn, cursor = Data.open_DB("test_db.sqlite")
+    Data.setup_DB_schools(cursor)
+    Data.setup_DB_jobs(cursor)
 
-    #get the stuff from the DB
-    query = cursor.execute('''SELECT * from schools''').fetchall()
+    # make a test entry to put into the DB
+    # need Name, City, 2018 size, 2017 size, 2017 earnings, 2016 repayment
+    test_api_data = [{'school.name': 'Test University', 'school.city': 'Boston', '2018.student.size': 1000,
+                  '2017.student.size': 900,
+                  '2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line': 456,
+                  '2016.repayment.3_yr_repayment.overall': 4004}]
 
-    #now compare to expected values
-    for x in range(len(data)):
-        assert query[x] == data[x]
+    # make a test entry for the jobs table and put into DB
+    test_excel_data = [{'area_title': 'Mass', 'occ_title': 'Management', 'o_group': 'major', 'tot_emp': 7000,
+                        'h_pct25': 25.89, 'a_pct25': 50000, 'occ_code': '11-7890'}, {'area_title': 'Maine',
+                        'occ_title': 'Developer', 'o_group': 'random', 'tot_emp': 800, 'h_pct25': 30.78,
+                        'a_pct25': 100000, 'occ_code': '45-3410'}, {'area_title': 'Iowa', 'occ_title': 'Farmer',
+                        'o_group': 'major', 'tot_emp': 1000, 'h_pct25': 20.58, 'a_pct25': 40000,
+                        'occ_code': '32-7591'}]
+
+    # now put it into the DB
+    Data.store_In_DB(test_api_data, test_excel_data, cursor)
+
+    # close DB
+    Data.close_DB(conn)
+
+    # now check if it is in DB
+    conn, cursor = Data.open_DB("test_db.sqlite")
+
+    # get the stuff from the DB
+    # check school table first
+
+    # the sqlite_master table is a metadata table with information about all the tables in it
+    cursor.execute('''SELECT name FROM sqlite_master
+    WHERE type ='table' AND name LIKE 'school_%';''')
+    # like does pattern matching with % as the wildcard
+
+    res = cursor.fetchall()
+    assert len(res) == 1
+
+    cursor.execute('''SELECT school_name FROM schools''')
+    res = cursor.fetchall()
+    test_res = res[0]  # seems to et rid of ('',) from res
+    assert test_res[0] == 'Test University'
+
+    # now for jobs table
+    cursor.execute('''SELECT name FROM sqlite_master
+    WHERE type ='table' AND name LIKE 'occu_%';''')
+
+    # does not work...
+    res = cursor.fetchall()
+    assert len(res) == 1
+
+    # this works
+    cursor.execute('''SELECT state_name FROM occupation''')
+    res = cursor.fetchall()
+    test_eres = res[0]
+    assert test_eres[0] == 'Mass'
+    test_eres = res[1]
+    assert test_eres[0] == 'Iowa'
 
     Data.close_DB(conn)
