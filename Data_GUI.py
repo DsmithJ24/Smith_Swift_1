71f908d23f1d84f219dfa577d0f9a4eebc9ad09a
RafaGM24 <DsmithJ24@gmail.com>
2021-03-16 00:33:26 -0400

    GUI can now take an input from the user and search for the input as a file name. If no file name exists, displays a message. If the file does exist, displays a different message and displays data. Right now only a simple test data is shown and data is not retrieved yet.

--- Data_GUI.py
+++ Data_GUI.py
@@ -1,3 +1,5 @@
+import os
+
 import PySide6.QtWidgets
 import sys
 import Data
@@ -9,18 +11,30 @@ from typing import List, Dict
 
 # This file seems to take in data. other file does the stuff with the data
 
-def display_data(data: list):
-    qt_app = PySide6.QtWidgets.QApplication(sys.argv)
-    my_window = Data_Window.MainWindow(data)
-    sys.exit(qt_app.exec_())
 
+def display_data():
+    # ToDo: have this return data from get data and return it
+    '''
+    qt_app = PySide6.QtWidgets.QApplication(sys.argv)
+    my_window = Data_Window.MainWindow()
+    sys.exit(qt_app.exec_())
+    '''
+    received_data = get_data()
+    return received_data
+
+
+def find_file(file_name:str):
+    pre = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(pre, file_name)
+    try:
+        file = open(path)
+        file.close()
+        return True
+    except FileNotFoundError:
+        return False
 
 def get_data() -> List[Dict]:
-
-    # ToDO: when updating let user choose a file name or just take excel file.
-    #  Later in project, make it so the wb name is a variable that takes an input.
-    #  This data will be from sprint 3 Database, not the contents of the file itself
-
+    '''
     # ToDo: pass a file name into this
     Data.main("state_M2019_dl.xlsx")
 
@@ -28,11 +42,16 @@ def get_data() -> List[Dict]:
     conn.row_factory = sqlite3.Row
     database = conn.cursor()
 
-    school_rows = database.execute('''SELECT * FROM schools''').fetchall()
-    job_rows = database.execute('''SELECT * FROM occupation''').fetchall()
+    school_rows = database.execute(''
+    'SELECT * FROM schools'
+    '').fetchall()
+    job_rows = database.execute(''
+    'SELECT * FROM occupation'
+    '').fetchall()
 
     conn.commit()
     conn.close()
+
 
     #schools = json.loads(school_rows)
     #jobs = json.loads(job_rows)
@@ -41,38 +60,48 @@ def get_data() -> List[Dict]:
     #  and jobs (occ_code not starting with 30-39 or 40-49) plus 25% percent salary from jobs table
 
     data_list = []
+    school_list = []
+    job_list = []
 
     for sdata in school_rows:
         size_total = sdata['student_size_2018']
-        #size_total = size_cell.value
-        if not isinstance(size_total, numbers.Number):
-            continue
-
         declining_balance = sdata['repayment_declining_2016']
-        if not isinstance(declining_balance, numbers.Number):
-            continue
-        record = {"total students": size_total, "3 year balance": declining_balance}
-        data_list.append(record)
+        school_state = sdata['school_state']
+        record = {"state": school_state, "total students": size_total, "3 year balance": declining_balance}
+        school_list.append(record)
 
     # ToDO these dont seem to be added to the datalist....
     for jdata in job_rows:
         ooc_codes = jdata['occupation_code']
         hourly_salary = jdata['hour_salary_25th_percentile']
-        record = {"occ_code": ooc_codes, "hourly_salary": hourly_salary}
-        data_list.append(record)
+        job_state = jdata['state_name']
+        record = {"state": job_state, "occ_code": ooc_codes, "hourly_salary": hourly_salary}
+        job_list.append(record)
 
-    return data_list
+    '''
 
-'''
+    test_list = []
+
+    record = {"total students": 100, "3 year balance": 3000, "occ_code": 45-679, "hourly salary": 15}
+    test_list.append(record)
+    return test_list
+
+
 def get_key(value:dict):
     return value["hourly_salary"]
-'''
+
 
 # ToDo: next two funcs may be changed and/or put in Data.py
 def main():
+    '''
     data = get_data()
-    # data.sort(key=get_key)  # sorts from highest salary to
+    #school_data.sort(key=get_key)  # sorts from highest salary to
+    #job_data.sort(key=get_key())
     display_data(data)
+    '''
+    qt_app = PySide6.QtWidgets.QApplication(sys.argv)
+    my_window = Data_Window.MainWindow()
+    sys.exit(qt_app.exec_())
 
 
 if __name__ == '__main__':
--- Data_Window.py
+++ Data_Window.py
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import QMessageBox, QWidget, QPushButton, QListWidget, QApplication, QListWidgetItem
+from PySide6.QtWidgets import QMessageBox, QWidget, QPushButton, QListWidget, QApplication, QListWidgetItem, QLineEdit
 from typing import List, Dict
+import Data_GUI
 
 
 class SearchWindow(QWidget):
@@ -7,60 +8,66 @@ class SearchWindow(QWidget):
         super().__init__()
         self.from_main = main_self
         self.setup_window()
+        self.file_name = ""
 
     def setup_window(self):
         self.setWindowTitle("Select Data")
-        self.setGeometry(150, 150, 300, 200)
+        self.setGeometry(150, 150, 300, 100)
+        search_bar = QLineEdit(self)
+        search_bar.resize(250, 25)
+        search_bar.move(25, 25)
+        search_bar.textChanged.connect(self.save_file_name)
 
         quit_button = QPushButton("Exit", self)
         quit_button.clicked.connect(self.close)
         quit_button.resize(quit_button.sizeHint())
-        quit_button.move(100, 150)
+        quit_button.move(100, 70)
 
-        # ToDo: rename this later to search, there will be no default
-        default_button = QPushButton("Use Default", self)
+        default_button = QPushButton("Search", self)
         default_button.resize(default_button.sizeHint())
-        default_button.move(200, 150)
-        default_button.clicked.connect(self.show_message)
+        default_button.move(200, 70)
+        default_button.clicked.connect(self.find_file)
         self.show()
 
-    def show_message(self):
-        message_box = QMessageBox(self)
-        message_box.setWindowTitle("Message")
-        message_box.setText("Default Selected!!!")
-        '''
-        message_box.setStandardButtons(QMessageBox.Ok)
-        return_value = message_box.exec_()
-        if return_value == QMessageBox.Ok:
-            self.use_default()
-        '''
-        message_box.show() # shows the message box itself
-    '''
-    def use_default(self):
+    def save_file_name(self, text):
+        self.file_name = text
+
+    def find_file(self):
+        if Data_GUI.find_file(self.file_name) is True:
+            self.show_good_message()
+        else:
+            self.show_error_message()
+
+    def show_good_message(self):
+        # ToDo: do the get data func in Data_GUI and pass in the file name
         MainWindow.show_data(self.from_main)
+        message_box = QMessageBox(self)
+        message_box.setWindowTitle("Success")
+        message_box.setText("Data Retrieved.")
+        message_box.setStandardButtons(QMessageBox.Ok)
+        message_box.show()  # shows the message box itself
         self.close()
-    '''
+
+    def show_error_message(self):
+        message_box = QMessageBox(self)
+        message_box.setWindowTitle("Failure")
+        message_box.setText("File does not exist. Please enter a different file name.")
+        message_box.show()
+
 
 class MainWindow(QWidget):
-    def __init__(self, data_to_show):
+    #def __init__(self, data_to_show):
+    def __init__(self):
         super().__init__()
-        self.data = data_to_show
+        self.data = None
         self.list_control = None
         self.reference_window = None
         self.setup_window()
+        self.display_list = None
 
     def setup_window(self):
         self.setWindowTitle("Data_Window")
-
-        # ToDo: next few lines should be done once data has been chosen
-        display_list = QListWidget(self)
-
-        self.list_control = display_list
-        self.put_data_in_list(self.data)
-        display_list.resize(400, 350)
-
         self.setGeometry(100, 100, 600, 500)
-
         quit_button = QPushButton("Exit", self)
         quit_button.clicked.connect(QApplication.instance().quit)
         quit_button.resize(quit_button.sizeHint())
@@ -79,30 +86,27 @@ class MainWindow(QWidget):
 
     def put_data_in_list(self, data: List[Dict]):
         for item in data:
-            display_item = f"{item['total students']}\t\t{item['3 year balance']}\t\t{item['occ_code']}" \
-                           f"\t\t{item['hourly salary']}"
-            list_item = QListWidgetItem(display_item, listview=self.list_control)
+            display_item1 = f"{item['total students']}\t\t{item['3 year balance']}\t\t{item['occ_code']}" \
+                            f"\t\t{item['hourly salary']}"
+            list_item1 = QListWidgetItem(display_item1, listview=self.list_control)
 
     def update_data(self):
-        # ToDO: figure out how to make a second regular GUI pop up, not a message box
         if self.reference_window is None:
             self.reference_window = SearchWindow(self)
             self.reference_window.show()
         else:
             self.reference_window.close()
             self.reference_window = None
-            #self.show_data()
 
-        # ToDO: when updating the data, take a user input for a file. Bring up GUI that has either default button
-        #  or search bar with search button
-    '''
     def show_data(self):
-        display_list = QListWidget(self)
-        self.list_control = display_list
+        self.data = Data_GUI.display_data()
+        self.display_list = QListWidget(self)
+        self.list_control = self.display_list
         self.put_data_in_list(self.data)
-        display_list.resize(400, 350)
-        self.show()
-    '''
+        self.display_list.resize(400, 350)
+        self.display_list.show() # show this widget, not the whole thing
+        print("Got here")
+
     def data_visualization(self):
         message_box = QMessageBox(self)
         message_box.setWindowTitle("Message")
@@ -111,7 +115,6 @@ class MainWindow(QWidget):
 
     # ToDO: Create a GUI for the data
     #  GUI should allow user to update the data or visualize data
-
 
     # ToDo: For visualization, have two forms of data analysis
     #  1) display data in color coded text format as a list in ascending or descending order (user chooses)
